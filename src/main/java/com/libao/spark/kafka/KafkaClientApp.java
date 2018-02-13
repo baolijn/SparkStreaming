22ac9fdcb6be539c5ec3801adcf20fd5cf5170e0
baolijn <libao_fighting@163.com>
2018-02-13 14:22:46 +0800

    add kafkaConsumer

--- src/main/java/com/libao/spark/kafka/KafkaClientApp.java
+++ src/main/java/com/libao/spark/kafka/KafkaClientApp.java
@@ -3,5 +3,6 @@ package com.libao.spark.kafka;
 public class KafkaClientApp {
     public static void main(String[] args) {
         new KafkaProducer(KafkaProperties.TOPIC).start();
+        new KafkaConsumer(KafkaProperties.TOPIC).start();
     }
 }
