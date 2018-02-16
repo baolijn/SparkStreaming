f515b11a8887b7a7db1d9e031a5a0f666749fba8
baolijn <libao_fighting@163.com>
2018-02-16 11:39:14 +0800

    add demo

---
+++ src/main/scala/com/libao/spark/FileWordCount.scala
@@ -0,0 +1,19 @@
+package com.libao.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * 使用spark streaming处理文件系统(local or hdfs)的数据
+  */
+object FileWordCount {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local").setAppName("FileWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+    val lines = ssc.textFileStream("F://text/")
+    val result = lines.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+    result.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
